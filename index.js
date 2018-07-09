4098fe196df4c9f3513a642a2eb854d4f2a05ebf
avkol <avlkol@yandex.ua>
2018-07-09 14:16:39 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // учимся создавать телеграм бота по уроку: https://www.youtube.com/watch?v=IuUY-OJ0GXE
 
+const http = require('http');
+
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = '572769275:AAEmfiIr-opFqke6s1q-6XlSxCdPbJ1kSqg'; //avkolTestBot - Бот-испытатель-2
@@ -7,5 +9,8 @@ const token = '572769275:AAEmfiIr-opFqke6s1q-6XlSxCdPbJ1kSqg'; //avkolTestBot - 
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', (msg) =>{
-	bot.sendMessage(msg.chat.id, `Hi, ${msg.from.first_name} from my bot!`)
+	bot.sendMessage(msg.chat.id, `Здравствуйте, ${msg.from.first_name}! Вы написали мне "${msg.text.toString()}", но я не знаю, что это значит :(`)
 });
+
+http.createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+    res.end('');
