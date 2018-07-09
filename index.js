02600d33bbb1133b0922424bb6d17119fe9549b7
avkol <avlkol@yandex.ua>
2018-07-09 14:40:24 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const token = '572769275:AAEmfiIr-opFqke6s1q-6XlSxCdPbJ1kSqg'; //avkolTestBot - 
 
 const bot = new TelegramBot(token, {polling: true});
 
+
+
 bot.onText(/\/start/, (msg) => {
 
 bot.sendMessage(msg.chat.id, "Welcome");
@@ -15,9 +17,17 @@ bot.sendMessage(msg.chat.id, "Welcome");
 });
 
 bot.on('message', (msg) =>{
-	bot.sendMessage(msg.chat.id, `Здравствуйте, ${msg.from.first_name}! Вы написали мне "${msg.text.toString()}", но я не знаю, что это значит :(`)
+	
+	if (msg.text == '\start'){
+	bot.sendMessage(msg.chat.id, `Здравствуйте, ${msg.from.first_name}! Рад Вас приветствовать! \n Позвольте представиться, я - бот-испытатель, \n 
+	моя задача состоит в том, чтобы тестировать функции бота в телеграме. `)
+	}
+	
+	
+	bot.sendMessage(msg.chat.id, `${msg.from.first_name}, прошу прощения! Вы написали мне "${msg.text.toString()}", но я не знаю, что это значит :(`)
 });
+
 /**
 http.createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
     res.end('');
-    */
+	*/
