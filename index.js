4ceab88a7b76f861d248af746f34cb5a6bfcf81a
avkol <avlkol@yandex.ua>
2018-12-17 20:22:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ http.createServer().listen(process.env.PORT || 5000).on('request', function(req,
 
 var server = http.createServer(function(req, res){
 
-   var reloaded = '<script>var myReload = function(){window.location.reload(true)}; setInterval(myReload(), 5000)</script>';
+   //var reloaded = '<script>var myReload = function(){window.location.reload(true)}; setInterval(myReload(), 5000);</script>';
+  var reloaded = '<script>var myReload = function(){alert("XXX"), 5000)}</script>';	
    var arrayOfGrittings = ['One','Two','Three','Four'];	
    function getRandomInt(min, max) {
              return Math.floor(Math.random() * (max - min)) + min;
@@ -48,7 +49,7 @@ var server = http.createServer(function(req, res){
    var greeting = arrayOfGrittings[rand];	
    
 	res.writeHead(200, {"Content-type":"Text/html"});
-	res.end(reloaded+'<center><h1>Hello World <script>document.write(greeting)</script></h1></center>');
+	res.end(reloaded+'<center><h1>Hello World</h1></center> <script>document.write(greeting)</script>');
 });
 
 server.listen(process.env.PORT, function(){console.log('Server is running')});
